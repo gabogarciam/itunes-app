52e24482c527aaca18a192d64a124ed417905f9b
gabogarciam <gabogarciam71@gmail.com>
2018-10-11 02:23:29 +0200

    server added

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/itunes-app'));
+
+app.get('*', (req, res, nex) => {
+    res.sendfile(path.join(__dirname + '/dist/itunes-app/index.html'));
+});
+
+app.listen(process.env.PORT || 3000);
